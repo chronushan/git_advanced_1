a98b6280f4ea31f127f85a390571ed218cf706b5
chronushan <48421873+chronushan@users.noreply.github.com>
2023-03-30 20:01:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 def even_list(lst):
-        if x%2 == 0:
-                return (x for x in lst)
+        #TODO
 
 
 def sum_of_squares_of_even(lst):
-        if x%2 == 0:
-                return sum(x**2 for x in lst)
+        #TODO
 
                         
